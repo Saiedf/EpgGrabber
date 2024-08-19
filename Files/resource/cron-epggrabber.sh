e14ab1baa28c7bffc2e3017068677415ce95a920
Saiedf <92365793+Saiedf@users.noreply.github.com>
2024-08-20 00:22:51 +0300

    Add files via upload

--- Files/resource/cron-epggrabber.sh
+++ Files/resource/cron-epggrabber.sh
@@ -1,23 +1,31 @@
 #!/bin/sh
 
+# Clear the cache memory
 echo 1 > /proc/sys/vm/drop_caches
 echo 2 > /proc/sys/vm/drop_caches
 echo 3 > /proc/sys/vm/drop_caches
 
+# Execute the Python scripts sequentially
+python /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/elcinmaiet5.py &
+wait
+python /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/beinsportiet5.py &
+wait
+python /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/nilesatiet5.py &
+wait
+python /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/sportiet5.py &
+wait
+python /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/uaeariet5.py &
+wait
+python /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/uaeeniet5.py &
+wait
+python /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/beincin.py &
+wait
+python /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/beinConnect.py &
+wait
+python /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/elcin.py &
+wait
+python /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/elcinEN.py &
+wait
 
-python /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/bebawy6.py
-wait 
-python /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/beincin.py
-wait
-python /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/beinConnect.py
-wait
-python /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/elcin.py
-wait
-python /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/elcinEN.py
-wait
-python /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/elcinmaiet5.py
-wait
-python /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/jawwyenOS.py
-python /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/sportiet5.py
-
+# Exit the script
 exit 0
