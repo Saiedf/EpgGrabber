b502bbd7b189186edaf733968918a8a4d6961dfe
Saiedf <92365793+Saiedf@users.noreply.github.com>
2024-12-27 16:32:48 +0200

    Add files via upload

--- Files/EPGGrabber/core/check.py
+++ Files/EPGGrabber/core/check.py
@@ -98,3 +98,45 @@ if not os.path.exists('/etc/epgimport/ziko_config/beinsportiet5.channels.xml'):
     beinsportiet5_channels = requests.get('https://github.com/ziko-ZR1/Epg-plugin/blob/master/configs/beinsportiet5.channels.xml?raw=true')
     with io.open('/etc/epgimport/ziko_config/beinsportiet5.channels.xml', 'w', encoding="utf-8") as f:
         f.write(beinsportiet5_channels.text)
+
+if not os.path.exists('/etc/epgimport/ziko_config/dstv.channels.xml'):
+    print('Downloading dstv channels config')
+    sys.stdout.flush()
+    dstv_channels = requests.get('https://github.com/ziko-ZR1/Epg-plugin/blob/master/configs/dstv.channels.xml?raw=true')
+    with io.open('/etc/epgimport/ziko_config/dstv.channels.xml', 'w', encoding="utf-8") as f:
+        f.write(dstv_channels.text)
+
+if not os.path.exists('/etc/epgimport/ziko_config/freesat.channels.xml'):
+    print('Downloading freesat channels config')
+    sys.stdout.flush()
+    free_channels = requests.get('https://github.com/ziko-ZR1/Epg-plugin/blob/master/configs/freesat.channels.xml?raw=true')
+    with io.open('/etc/epgimport/ziko_config/freesat.channels.xml', 'w', encoding="utf-8") as f:
+        f.write(free_channels.text)
+
+if not os.path.exists('/etc/epgimport/ziko_config/skyit.channels.xml'):
+    print('Downloading skyit channels config')
+    sys.stdout.flush()
+    sky_it = requests.get('https://github.com/ziko-ZR1/Epg-plugin/blob/master/configs/skyit.channels.xml?raw=true')
+    with io.open('/etc/epgimport/ziko_config/skyit.channels.xml', 'w', encoding="utf-8") as f:
+        f.write(sky_it.text)
+
+if not os.path.exists('/etc/epgimport/ziko_config/discovery.channels.xml'):
+    print('Downloading discovery.channels config')
+    sys.stdout.flush()
+    discovery_channels = requests.get('https://github.com/ziko-ZR1/Epg-plugin/blob/master/configs/discovery.channels.xml?raw=true')
+    with io.open('/etc/epgimport/ziko_config/discovery.channels.xml', 'w', encoding="utf-8") as f:
+        f.write(discovery_channels.text)
+
+if not os.path.exists('/etc/epgimport/ziko_config/snrt.channels.xml'):
+    print('Downloading Snrt.channels config')
+    sys.stdout.flush()
+    snrt_channels = requests.get('https://github.com/ziko-ZR1/Epg-plugin/blob/master/configs/snrt.channels.xml?raw=true')
+    with io.open('/etc/epgimport/ziko_config/snrt.channels.xml', 'w', encoding="utf-8") as f:
+        f.write(snrt_channels.text)
+
+if not os.path.exists('/etc/epgimport/ziko_config/satTv.channels.xml'):
+    print('Downloading satTv.channels config')
+    sys.stdout.flush()
+    satTv_channels = requests.get('https://github.com/ziko-ZR1/Epg-plugin/blob/master/configs/satTv.channels.xml?raw=true')
+    with io.open('/etc/epgimport/ziko_config/satTv.channels.xml', 'w', encoding="utf-8") as f:
+        f.write(satTv_channels.text)
