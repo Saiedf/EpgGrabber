bc012be0ccd78d8d87aae1730fb63057ee870fa1
Saiedf <92365793+Saiedf@users.noreply.github.com>
2025-02-17 15:30:30 +0200

    Add files via upload

---
+++ Files/TIME_PLUS_3/provider_plus_3.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Download provider_plus_3.00 Time
+
+wget -O /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/poland2iet5.py https://raw.githubusercontent.com/Saiedf/EpgGrabber/refs/heads/main/Files/TIME_PLUS_3/poland2iet5.py
+sleep 2
+
+wget -O /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/qatar1iet5.py https://raw.githubusercontent.com/Saiedf/EpgGrabber/refs/heads/main/Files/TIME_PLUS_3/qatar1iet5.py
+sleep 2sleep 2sleep 2sleep 2sleep 2
+
+wget -O /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/qatar2iet5.py https://raw.githubusercontent.com/Saiedf/EpgGrabber/refs/heads/main/Files/TIME_PLUS_3/qatar2iet5.py
+sleep 2sleep 2sleep 2sleep 2
+
+wget -O /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/qatar3iet5.py https://raw.githubusercontent.com/Saiedf/EpgGrabber/refs/heads/main/Files/TIME_PLUS_3/qatar3iet5.py
+sleep 2sleep 2sleep 2
+
+wget -O /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/qatar4iet5.py https://raw.githubusercontent.com/Saiedf/EpgGrabber/refs/heads/main/Files/TIME_PLUS_3/qatar4iet5.py 
+sleep 2sleep 2
+
+wget -O /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/qatar5iet5.py https://raw.githubusercontent.com/Saiedf/EpgGrabber/refs/heads/main/Files/TIME_PLUS_3/qatar5iet5.py
+sleep 2
+
+wget -O /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/saudiarabia1iet5.py https://raw.githubusercontent.com/Saiedf/EpgGrabber/refs/heads/main/Files/TIME_PLUS_3/saudiarabia1iet5.py
+sleep 2
+
+wget -O /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/saudiarabia2iet5.py https://raw.githubusercontent.com/Saiedf/EpgGrabber/refs/heads/main/Files/TIME_PLUS_3/saudiarabia2iet5.py
+
+wget -O /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/saudiarabia3iet5.py https://raw.githubusercontent.com/Saiedf/EpgGrabber/refs/heads/main/Files/TIME_PLUS_3/saudiarabia3iet5.py
+sleep 2
+
+wget -O /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/saudiarabia4iet5.py https://raw.githubusercontent.com/Saiedf/EpgGrabber/refs/heads/main/Files/TIME_PLUS_3/saudiarabia4iet5.py
+sleep 2
+
+wget -O /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/uae1iet5.py https://raw.githubusercontent.com/Saiedf/EpgGrabber/refs/heads/main/Files/TIME_PLUS_3/uae1iet5.py
+sleep 2sleep 2sleep 2sleep 2
+
+wget -O /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/uae2iet5.py https://raw.githubusercontent.com/Saiedf/EpgGrabber/refs/heads/main/Files/TIME_PLUS_3/uae2iet5.py
+sleep 2sleep 2sleep 2
+
+wget -O /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/uae3iet5.py https://raw.githubusercontent.com/Saiedf/EpgGrabber/refs/heads/main/Files/TIME_PLUS_3/uae3iet5.py
+sleep 2sleep 2
+
+wget -O /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/uae4iet5.py https://raw.githubusercontent.com/Saiedf/EpgGrabber/refs/heads/main/Files/TIME_PLUS_3/uae4iet5.py
+sleep 2
+
+wget -O /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/egypt2iet5.py https://raw.githubusercontent.com/Saiedf/EpgGrabber/refs/heads/main/Files/TIME_PLUS_3/egypt2iet5.py
+sleep 2
+
+wget -O /usr/lib/enigma2/python/Plugins/Extensions/EPGGrabber/providers/poland1iet5.py https://raw.githubusercontent.com/Saiedf/EpgGrabber/refs/heads/main/Files/TIME_PLUS_3/poland1iet5.py
+sleep 2
